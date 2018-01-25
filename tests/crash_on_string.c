1b77972c2fdde6927bcac979ab4dc0e19dab6d26
Matt Borgerson <contact@mborgerson.com>
2018-01-24 18:02:13 -0700

    Fix interesting detector

--- tests/crash_on_string.c
+++ tests/crash_on_string.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 #include <string.h>
 
+int loop_a_bunch()
+{
+    int x = 20;
+    for (volatile int i = 0; i < 1000; i++) {
+        x += 1;
+    }
+
+    return x;
+}
+
 int do_crash()
 {
     *(volatile char *)(0x123) = 5;
@@ -16,7 +26,10 @@ int main(int argc, char *argv[])
     size_t len = ftell(fd);
     fseek(fd, 0, SEEK_SET);
     fread(buf, 1, len, fd);
-    if (strcmp(buf, "Hello\x21World") == 0) {
+    if (strcmp(buf, "Hello Worl\xE4") == 0) {
+        loop_a_bunch();
+    }
+    if (strcmp(buf, "Hell\xEF World") == 0) {
         do_crash();
     }
     printf("%s\n", buf);
