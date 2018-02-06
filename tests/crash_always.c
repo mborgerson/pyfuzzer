ae0af6080db56136702fea6a48d37ce1cbb5d906
Matt Borgerson <contact@mborgerson.com>
2018-02-05 22:39:39 -0700

    Remove my tests

--- tests/crash_always.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main(int argc, char *argv[])
-{
-    printf("%x\n", *(char *)(0x123));
-    return 0;
-}
--- tests/crashme.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main(int argc, char *argv[])
-{
-    //printf("%x\n", *(char *)(0x123));
-    puts("Hi\n");
-    // while (1);
-    return 0;
-}
